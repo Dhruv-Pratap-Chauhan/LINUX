73fc69f042ce016102f4d841673ffab704c182b5
Dhruv-Pratap-Chauhan <dhruvpratapchauhan7@gmail.com>
2025-10-21 19:36:42 +0530

    ok

---
+++ exp9.sh
@@ -0,0 +1,15 @@
+#write a script that displays various system information like curret date and time , logges in users, system architecture information.
+
+echo "Current Date and Time: $(date)"
+echo "Logged-in Users:"
+who  
+echo "Uptime:"
+uptime
+echo "User Name: $USER"
+echo "System Hostname $HOSTNAME"
+echo "System Architecture Information:"
+uname -m
+echo "Disk Usage:"
+df -h
+echo "Memory Usage:"
+free -h
